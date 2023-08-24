6f024efef5986a05a72f976291281f16c778fa3e
edgarneto12 <edgarneto789@gmail.com>
2023-08-24 20:28:31 -0300

    Update aula02.java

--- Java/projetosjava/Linguagem de Programação I p2/Exercicios/src/aula02.java
+++ Java/projetosjava/Linguagem de Programação I p2/Exercicios/src/aula02.java
@@ -10,19 +10,25 @@ public class aula02 {
         System.out.println("------------------------ \nBem Vindo ao Menu da Jampa pets!!! \n[1] Comprar nossos produtos \n[2] Solicitar o valor do produto ou serviço \n[3] Falar com um atendente");
         // Imprime escolha de menu
         System.out.print("Digite o número de acordo com a sua escolha: ");
-        // define a variavel menu
+        // Define a entrada do ususario na variavel escolha_do_ususario
         escolha_do_ususario = entrada.nextInt();
+        // Fecha o leitor
         entrada.close();
+        // Inicia condição de acordo com a entrada
         switch (escolha_do_ususario){
+            // Caso o usuario escolha a opção 1 
             case 1:
                 System.out.println("Opção 1 escolhida.");
                 break;
+            // Caso o usuario escolha a opção 2
             case 2:
                 System.out.println("Opção 2 escolhida.");
                 break;
+            // Caso o usuario escolha a opção 3
             case 3:
                 System.out.println("Opção 3 escolhida.");
                 break;
+            // Caso o usuario escolha nenhuma opção ou inexistente
             default:
                 System.out.println("Opção invalida! ");
         }
