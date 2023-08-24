cf04b4f36f4d53fb9f9686f987b42f194d47bbf5
edgarneto12 <edgarneto789@gmail.com>
2023-08-24 20:23:38 -0300

    Update aula02.java

--- Java/projetosjava/Linguagem de Programação I p2/Exercicios/src/aula02.java
+++ Java/projetosjava/Linguagem de Programação I p2/Exercicios/src/aula02.java
@@ -2,10 +2,15 @@ import java.util.Scanner;
 
 public class aula02 {
     public static void main(String[] args) {
+        // Inicia o leitor da entrada do sistema
         Scanner entrada = new Scanner(System.in);
+        // Inica a variavel escolha_do_ususario
         int escolha_do_ususario;
+        // Imprime mensagem de boas vindas e menu
         System.out.println("------------------------ \nBem Vindo ao Menu da Jampa pets!!! \n[1] Comprar nossos produtos \n[2] Solicitar o valor do produto ou serviço \n[3] Falar com um atendente");
+        // Imprime escolha de menu
         System.out.print("Digite o número de acordo com a sua escolha: ");
+        // define a variavel menu
         escolha_do_ususario = entrada.nextInt();
         entrada.close();
         switch (escolha_do_ususario){
@@ -18,6 +23,8 @@ public class aula02 {
             case 3:
                 System.out.println("Opção 3 escolhida.");
                 break;
+            default:
+                System.out.println("Opção invalida! ");
         }
 
     }
