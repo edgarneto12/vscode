d8781bbf673145911f911527de232b576c0f872d
edgarneto12 <edgarneto789@gmail.com>
2023-08-30 19:16:53 -0300

    Create ex01.py

---
+++ faculdade/p2/estrutura-de-dados/aula3008/ex01.py
@@ -0,0 +1,9 @@
+valor_compra = float(input("Digite o valor da compra: "))
+idade_cliente = int(input("Digite sua idade: "))
+
+if idade_cliente >= 60:
+    valor_desconto = valor_compra * 0.15
+else:
+    valor_desconto = valor_compra * 0.1
+valor_final = valor_compra - valor_desconto
+print(f"O valor final da compra é {valor_final}")
