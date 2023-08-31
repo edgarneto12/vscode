010c403f22feabddbf21f11a240ddcddd5b09b14
edgarneto12 <edgarneto789@gmail.com>
2023-08-31 20:12:29 -0300

    Update ex06.java

--- faculdade/p2/Linguagem de Programação I/Exercicios/src/ex06.java
+++ faculdade/p2/Linguagem de Programação I/Exercicios/src/ex06.java
@@ -2,12 +2,13 @@ import java.util.Scanner;
 
 public class ex06 {
     public static void main(String[] args) {
+        soma p = new soma();
         Scanner entrada = new Scanner(System.in);
         System.out.print("Digite um número inteiro: ");
-        soma p = new soma();
         p.n1 = entrada.nextInt();
         System.out.print("Digite outro número inteiro: ");
         p.n2 = entrada.nextInt();
+        entrada.close();
         p.soman1n2();
         System.out.println(p.total);
     }
