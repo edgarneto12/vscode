4b95990ed1d22308ba7f4de685845ba9600c8ac2
edgarneto12 <edgarneto789@gmail.com>
2023-09-14 19:42:56 -0300

    update

---
+++ faculdade/p2/Linguagem de Programação I/Exercicios/src/aula1409/Pessoa.java
@@ -0,0 +1,15 @@
+package aula1409;
+public class Pessoa {
+    
+    private String nome;
+    private int idade;
+
+    public void setIdade(int idade) {
+        this.idade = idade;
+    }
+
+    public int getIdade() {
+        return idade;
+    }
+    // get e set para nome
+}
---
+++ faculdade/p2/Linguagem de Programação I/Exercicios/src/aula1409/Principal.java
@@ -0,0 +1,14 @@
+package aula1409;
+import java.util.Scanner;
+
+public class Principal {
+    public static void main(String[] args) {
+
+        Scanner sc = new Scanner(System.in);
+        Pessoa p = new Pessoa();
+
+        System.out.println("Digite sua idade");
+        p.setIdade(sc.nextInt());
+        System.out.println("Idade:"+ p.getIdade());
+    }
+}
